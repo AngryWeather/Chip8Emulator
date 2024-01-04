907e2404eb1e7e5159457c5f56a3f6e0945938c3
Weronika <WerRic96@protonmail.com>
2024-01-04 21:39:08 +0100

    Return error if no filename was given

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
+type NoFilenameError struct{}
+
 func main() {
 }
 
-func GetFilenameFromCommand(args []string) string {
-	return args[2]
+func (n NoFilenameError) Error() string {
+	return "no filename was given"
+}
+
+func GetFilenameFromCommand(args []string) (string, error) {
+	if len(args) < 3 {
+		return "", NoFilenameError{}
+	}
+	return args[2], nil
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"testing"
 )
@@ -8,7 +9,7 @@ import (
 func TestExtractCommandLineArguments(t *testing.T) {
 	t.Run("Extract filename from command", func(t *testing.T) {
 		os.Args = []string{"go", "main.go", "file.ch8"}
-		got := GetFilenameFromCommand(os.Args)
+		got, _ := GetFilenameFromCommand(os.Args)
 		want := "file.ch8"
 
 		assertFilename(t, got, want)
@@ -16,10 +17,32 @@ func TestExtractCommandLineArguments(t *testing.T) {
 	t.Run("Return 'test_file.ch8' from command", func(t *testing.T) {
 		os.Args = []string{"go", "main.go", "test_file.ch8"}
 
-		got := GetFilenameFromCommand(os.Args)
+		got, _ := GetFilenameFromCommand(os.Args)
 		want := "test_file.ch8"
 
 		assertFilename(t, got, want)
+	})
+	t.Run("Return error if no filename was given", func(t *testing.T) {
+		os.Args = []string{"go", "main.go"}
+
+		_, err := GetFilenameFromCommand(os.Args)
+
+		if err == nil {
+			t.Fatalf("expected an error")
+		}
+
+		var got NoFilenameError
+		isNoFilenameError := errors.As(err, &got)
+		want := NoFilenameError{}
+
+		if !isNoFilenameError {
+			t.Fatalf("was not a NoFilenameError got %T", err)
+		}
+
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+
 	})
 
 }
